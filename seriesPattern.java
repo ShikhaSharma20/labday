ccfbea85bfc738174eea437ac60a2eac18b87721
ShikhaSharma20 <109273450+ShikhaSharma20@users.noreply.github.com>
2022-07-22 13:00:53 +0530

    Add files via upload

---
+++ seriesPattern.java
@@ -0,0 +1,13 @@
+class seriesPattern{
+   public static void main(String arg[])
+{
+ //main method
+int num=9;
+//initialize the number
+for(int i=1;i<=12;i++){
+//for loop
+System.out.println(num);
+num +=10;
+}
+}
+}
