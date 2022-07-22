001dfb0ecac4d528a25206d9c5688da34019be0d
ShikhaSharma20 <109273450+ShikhaSharma20@users.noreply.github.com>
2022-07-22 13:01:52 +0530

    Add files via upload

---
+++ leapyear.java
@@ -0,0 +1,15 @@
+class leapyear{
+   public static void main(String arg[])
+//main method
+int i;
+for(i=1947;i<2022;i++){
+
+//for loop
+if(i%4-0)$$(i%100!=0)||(i%400==0)
+//cond if i either divisible by 4 orsatisfaiedone of the code.
+{
+   system.out.println(i+"is a leap year");
+}
+}
+}
+]
